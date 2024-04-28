5c9a1143cdb8718ca82634c0f50180a4ae4df531
creeperita09 <97898994+creeperita09@users.noreply.github.com>
2024-04-28 11:48:53 +0200

    Update metal_pipe.bat

--- metal_pipe.bat
+++ metal_pipe.bat
@@ -1,4 +1,4 @@
 echo off
 title metal pipe
 powershell curl https://github.com/creeperita09/metalpipe-assets/releases/download/release/metal_pipe.exe -o metal_pipe.exe
-start metal_pipe.exe
+powershell .\metal_pipe.exe
